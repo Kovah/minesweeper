6e63f7bc7cd0544c9b3b0434e255bebf62878c6d
Kovah <mail@kovah.de>
2019-12-17 21:18:08 +0100

    Add base for Vue with Vue CLI

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Minesweeper</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
+  };
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+    <p>Minesweeper Generator</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld'
+  };
+</script>
